98a559bcd1cac204a8ce003765a9bd3beac20c1c
vika <vikatekutova7@gmail.com>
2026-03-18 09:41:56 +0300

    Step 9: form validation in JavaScript

--- main.js
+++ main.js
@@ -45,3 +45,21 @@ form.addEventListener("submit", (event) => {
     const age = ageInput.value;
     result.textContent = `Имя: ${name}, Возраст: ${age}`;
 });
+const name = usernameInput.value.trim();
+const age = Number(ageInput.value);
+if (name === "") {
+    result.textContent = "Ошибка: Имя не может быть пустым!";
+    result.style.color = "red";
+    usernameInput.focus();
+    return;
+}
+if (isNaN(age) || age <=0 || age > 120) {
+    result.textContent = "Ошибка: Введите корректный вораст (от 1 до 120)!";
+    result.style.color = "red";
+    ageInput.focus();
+    ageInput.value = "";
+    return;
+}
+result.textContent = `Данные сохранены: ${name}, ${age} лет`;
+result.style.color = "green";
+form.reset();
